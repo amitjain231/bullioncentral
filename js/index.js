dc51a8cb31512533ef388ffdee5a6edca8468ff3
Amit kumar Jain <amitjain231@gmail.com>
2015-06-14 19:41:03 +0530

    Update 14062015
    Update 14062015

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-function initBullionCentral(){}function load_url(a){var e;switch(a){case"HOME":e="misc/home.html";break;case"VB":e="http://www.vardhamanbullion.com/live";break;case"RB":e="http://www.rahulbullion.com";break;case"CG":e="http://capsliverates.com/pc.php";break;case"SVBC":e="http://www.svbcgold.com/mobile";break;case"DP":e="http://dpbullion.in/"}$("#id_iframe_webbview").attr("src",e)}
+function initBullionCentral()
+{
+
+}
+	
+function load_url(pi_src)
+{
+	var lv_url;
+	
+	switch(pi_src) 
+	{
+		case "HOME":
+			lv_url = "misc/home.html";
+			break;
+		case "VB":
+			lv_url = "http://www.vardhamanbullion.com/live";
+			break;
+		case "RB":
+			lv_url = "http://www.rahulbullion.com";
+			break;
+		case "CG":
+			lv_url = "http://capsliverates.com/pc.php";
+			break;	
+		case "SVBC":
+			lv_url = "http://www.svbcgold.com/mobile";
+			break;	
+		case "DP":
+			lv_url = "http://dpbullion.in/";
+			break;
+		default:
+	}
+
+
+	$("#id_iframe_webbview").attr("src", lv_url);
+	
+}
+	
+	
